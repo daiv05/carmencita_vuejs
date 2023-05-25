dd7abcf077379a3cf50cb38198d6bdf609cff787
Luis-Rivas <71407850+Luis-Rivas@users.noreply.github.com>
2023-05-24 21:26:13 -0600

    Se agrega version inicial de agregar empleado
    add agregar empleado, y instalan vee-validate, axios, tailwindcss, headlessui y heroicons

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import EmpleadoAgregar from '../views/RecursosHumanos/EmpleadoAgregar.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/empleado_agregar',
+      name: 'empleado_agregar',
+      component: EmpleadoAgregar
     },
     {
       path: '/about',
