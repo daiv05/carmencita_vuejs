19bb35604e1c1608fb9926bbbb4e3104597174ba
Guardado <ga18097@ues.edu.sv>
2023-06-12 19:59:50 -0600

    Vista de prductos en tabla

--- src/views/Inventario/GestionProducto.vue
+++ src/views/Inventario/GestionProducto.vue
@@ -50,7 +50,7 @@ import NavBar from "../../components/NavBar.vue";
                             <thead>
                                 <tr
                                     class="text-md font-semibold tracking-wide text-gray-900 bg-gray-100 uppercase text-center">
-                                    <th class="px-4 py-3">Nombre Producto</th>
+                                    <th class="px-4 py-3">Producto</th>
                                     <th class="px-4 py-3">Código de Barras</th>
                                     <th class="px-4 py-3">Precio Unitario</th>
                                     <th class="px-4 py-3">Cantidad Disponible</th>
@@ -60,7 +60,21 @@ import NavBar from "../../components/NavBar.vue";
                             </thead>
 
                             <tbody class="bg-white">
-                                
+                                <tr class=" text-gray-700" v-for="producto in listaProductos" v-bind:key="producto.codigo_barra_producto">
+                                    <td class="cursor-pointer px-4 py-3 text-center casillaClick hover:bg-gray-100" @click="mostrarProducto(producto.id_producto)">
+                                        <div class="">
+                                            <p class="font-semibold text-black">{{ producto.nombre_producto }}</p>
+                                        </div>
+                                    </td>
+                                    <td class="px-4 py-3 text-ms font-semibold text-center">{{ producto.codigo_barra_producto }}</td>
+                                    <td class="px-4 py-3 text-ms font-semibold text-center">${{ producto.precio_unitario }}</td>
+                                    <td class="px-4 py-3 text-ms font-semibold text-center">{{ producto.cantidad_producto_disponible }}</td>
+                                    <td class="px-4 py-3 text-ms font-semibold text-center">{{ producto.esta_disponible }}</td>
+                                    <td class="px-4 py-3 text-xs text-center">
+                                        <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-emerald-500 dark:hover:bg-green-700 dark:focus:ring-green-800" @click="modificarProducto(producto.id_producto)">Editar</button>
+                                        <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 ml-10" @click="eliminarProducto(producto.id_producto)">Eliminar</button>
+                                    </td>
+                                </tr>                                
                             </tbody>
                         </table>
 
@@ -75,5 +89,57 @@ import NavBar from "../../components/NavBar.vue";
 </template>
 
 <script>
+
+import axios from 'axios';
+import {ref} from 'vue';
+
+export default{
+
+    data(){
+        return{
+            listaProductos: [],
+            filtro: 'activos',
+            listaProductosFiltrados: [],
+            productoSeleccionado: {},
+            exitoTransaccion: false,
+            mensajeTransaccion: '',
+        }
+    },
+    mounted(){
+        this.obtenerProductos();
+    },
+    methods:{
+        obtenerProductos(){
+            axios.get('http://127.0.0.1:8000/api/productos')
+            .then(response => {
+                this.listaProductos = response.data;
+                this.listaProductosFiltrados = response.data;
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        },
+        aplicarFiltro(){
+            if(this.filtro == 'activos'){
+                this.listaProductosFiltrados = this.listaProductos.filter(producto => producto.estado_producto == 1);
+            }else if(this.filtro == 'inactivos'){
+                this.listaProductosFiltrados = this.listaProductos.filter(producto => producto.estado_producto == 0);
+            }
+        },
+        agregarProducto(){
+            this.$router.push({name: 'agregarProducto'});
+        },
+        modificarProducto(id_producto){
+            this.$router.push({name: 'modificarProducto', params: {id_producto: id_producto}});
+        },
+        eliminarProducto(id_producto){
+            this.$router.push({name: 'eliminarProducto', params: {id_producto: id_producto}});
+        },
+        mostrarProducto(id_producto){
+            this.$router.push({name: 'mostrarProducto', params: {id_producto: id_producto}});
+        },
+    }
+
+}
 
 </script>
