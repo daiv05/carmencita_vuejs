73145d5ffc6878e0788640108755adfeaa126b31
juanBM-1122 <100696845+juanBM-1122@users.noreply.github.com>
2023-06-16 15:11:05 -0600

    correcciones en la funcion actualizar cargo

--- src/components/RecursosHumanos/ModalCargoComponent.vue
+++ src/components/RecursosHumanos/ModalCargoComponent.vue
@@ -12,7 +12,7 @@ import api_url from "../../config";
         <strong class="font-bold">Error a modificar Cargo</strong>
         <span class="block sm:inline">{{ this.mensajeConsulta }}</span>
         <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-        <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+        <svg @click = "errorConsulta=false" class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
         </span>
     </div>
       
@@ -102,8 +102,7 @@ export default{
               };
             },
             guardarModificacionEmpleado(values){
-              console.log("Hola mundo");
-              console.log(JSON.stringify(values, null, 2));
+              console.log("El cargo es : " +this.cargoModificado);
               axios.put(api_url+"/cargos/"+this.cargoModificado.id_cargo,this.cargoModificado)
               .then((response)=>{
                   console.log(response.data);
@@ -115,7 +114,13 @@ export default{
                   else{
                       this.errorConsulta = true;
                   }
-              });
+              }).catch(
+                response=>{
+                  this.errorConsulta = true;
+                  this.mensajeConsulta = response.data;
+                }
+
+              );
             },
             validarCamposDeTexto(value){
               if(!value){
