37fa09fef980a3a73ca2ba612982ce0a422e3505
daiv05 <dc19019@ues.edu.sv>
2023-06-18 13:25:31 -0600

    quitar prerender

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { basename } from 'node:path'
-import vitePrerender  from 'vite-plugin-prerender';
-import path from 'path'
+//import vitePrerender  from 'vite-plugin-prerender';
+//import path from 'path'
 
 // const path = require('path')
 // const PrerendererWebpackPlugin = require('@prerenderer/webpack-plugin')
@@ -22,12 +22,12 @@ import path from 'path'
 export default defineConfig({
   plugins: [
     vue(),
-    vitePrerender({
-      // Required - The path to the vite-outputted app to prerender.
-      staticDir: path.join(__dirname, 'dist'),
-      // Required - Routes to render.
-      routes: ['/registrar_nueva_venta', '/empleado_agregar', '/empleado_modificar/', '/listar_empleados', '/gestion_cargos', '/editar_producto/', '/agregar_producto', '/sales_list', '/detail_sales/', '/detail_cf/', '/gestion_productos', '/about', '/'],
-    }),
+    // vitePrerender({
+    //   // Required - The path to the vite-outputted app to prerender.
+    //   staticDir: path.join(__dirname, 'dist'),
+    //   // Required - Routes to render.
+    //   routes: ['/registrar_nueva_venta', '/empleado_agregar', '/empleado_modificar/', '/listar_empleados', '/gestion_cargos', '/editar_producto/', '/agregar_producto', '/sales_list', '/detail_sales/', '/detail_cf/', '/gestion_productos', '/about', '/'],
+    // }),
     // prerender({
     //   // Configura las rutas que deseas prerenderizar
     //   entries: ['/registrar_nueva_venta', '/empleado_agregar', '/empleado_modificar', '/listar_empleados', '/gestion_cargos', '/editar_producto', '/agregar_producto', '/sales_list', '/detail_sales', '/detail_cf', '/gestion_productos', '/about', '/'],
