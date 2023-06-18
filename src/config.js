227c28caaefad0f6f3460eb5eb6035983de9ad70
daiv05 <dc19019@ues.edu.sv>
2023-06-18 11:12:49 -0600

    quitar base + cambio url api

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // config.js
-const API_URL = "https://dsi-g05-carmencitaproject-production.up.railway.app/api";
+const API_URL = "https://carmencitalaravel-production.up.railway.app/api";
 export default API_URL;
