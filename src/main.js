00adeeeef7c1a352e37f897a9576ad06056d268a
David DV <71719478+daiv05@users.noreply.github.com>
2023-10-28 21:34:22 -0600

    api url

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ window.axios.defaults.headers.common["X-Requested-With"]="XMLHttpRequest";
 //axios.defaults.headers.common['Access-Control-Allow-Origin'] = window.location.origin;
 window.axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE';
 window.axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'X-Requested-With, Content-Type';
-window.axios.defaults.baseURL="http://localhost:8000"
+window.axios.defaults.baseURL="https://carmencitalaravel-production.up.railway.app"
 
 // Validar en cada response si el usuario esta autenticado
 window.axios.interceptors.response.use(
