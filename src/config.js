9526b39680fc58f83e81cacd758cbf6666dec80b
daderas <daderas@innovacion.gob.sv>
2024-05-22 10:00:23 -0600

    actualizando config base url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,3 @@
-// config.js
-//const API_URL = "https://carmencitalaravel-production.up.railway.app/api";
-//const API_URL = "http://localhost:8000/api";
-const API_URL = "http://localhost:8000/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api"; 
 
 export default API_URL;
