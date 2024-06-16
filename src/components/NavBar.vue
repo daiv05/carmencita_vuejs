8b1df11278e3a9179927b1d3416736fd027fce8a
David DV <71719478+daiv05@users.noreply.github.com>
2024-06-15 23:31:09 -0600

    arreglos navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -89,6 +89,7 @@ var navigation = [
   },
   {
     name: 'RRHH',
+    href: '/',
     rutas: rutasRRHH,
     current: false,
     roles: ['Gerente'],
@@ -96,6 +97,7 @@ var navigation = [
   },
   {
     name: 'Ventas',
+    href: '/',
     rutas: rutasVentas,
     current: false,
     roles: ['Gerente', 'Sub-Gerente'],
@@ -103,6 +105,7 @@ var navigation = [
   },
   {
     name: 'Inventario',
+    href: '/',
     rutas: rutasInventario,
     current: false,
     roles: ['Gerente', 'Sub-Gerente'],
@@ -110,6 +113,7 @@ var navigation = [
   },
   {
     name: 'Informes',
+    href: '/',
     rutas: rutasInformes,
     current: false,
     roles: ['Gerente'],
@@ -117,6 +121,7 @@ var navigation = [
   },
   {
     name: 'Registrar asistencia',
+    href: '/',
     rutas: rutasRegistrarAsistencia,
     current: false,
     roles: ['Sub-Gerente', 'Colaborador'],
@@ -309,19 +314,59 @@ quitarOpcionNavBar()
 
     <DisclosurePanel class="sm:hidden">
       <div class="space-y-1 px-2 pb-3 pt-2">
-        <router-link
-          v-for="item in navigation"
-          :key="item.name"
-          :to="item.href"
-          :class="[
-            item.current
-              ? 'bg-gray-900 text-white'
-              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-            'block rounded-md px-3 py-2 text-base font-medium'
-          ]"
-          :aria-current="item.current ? 'page' : undefined"
-          >{{ item.name }}</router-link
-        >
+        <div v-for="(item, index) in navigation" class="flex w-auto p-0 m-0">
+          <router-link
+            v-if="index == 0"
+            :key="item.name"
+            :to="item.href"
+            :class="[
+              item.current
+                ? 'bg-gray-900 text-white'
+                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+              'rounded-md px-3 py-2 text-sm font-medium'
+            ]"
+            :aria-current="item.current ? 'page' : undefined"
+            >{{ item.name }}</router-link
+          >
+          <Menu v-if="index > 0 && index < navigation.length" as="div" class="relative ml-3 my-2">
+            <div>
+              <MenuButton class="p-0">
+                <span
+                  :class="[
+                    item.current
+                      ? 'bg-gray-900 text-white'
+                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'rounded-md px-3 py-2 text-sm font-medium'
+                  ]"
+                  :aria-current="item.current ? 'page' : undefined"
+                  >{{ item.name }}</span
+                >
+              </MenuButton>
+            </div>
+            <transition
+              v-if="index > 0 && index < navigation.length"
+              enter-active-class="transition ease-out duration-100"
+              enter-from-class="transform opacity-0 scale-95"
+              enter-to-class="transform opacity-100 scale-100"
+              leave-active-class="transition ease-in duration-75"
+              leave-from-class="transform opacity-100 scale-100"
+              leave-to-class="transform opacity-0 scale-95"
+            >
+              <MenuItems
+                class="absolute z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+              >
+                <MenuItem v-slot="{ active }" v-for="ruta in item.rutas">
+                  <router-link
+                    :to="ruta.href"
+                    :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']"
+                  >
+                    {{ ruta.nombre }}</router-link
+                  >
+                </MenuItem>
+              </MenuItems>
+            </transition>
+          </Menu>
+        </div>
       </div>
     </DisclosurePanel>
   </Disclosure>
